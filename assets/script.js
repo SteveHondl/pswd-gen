8b3c6990ce1541be3ce12686671237152dfa849e
Steve Hondl <steve.hondl@gmail.com>
2023-11-11 16:16:54 -0600

    user prompts

--- assets/script.js
+++ assets/script.js
@@ -12,3 +12,34 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// My code
+
+function validatePasswordLength(length) {
+    
+  return length >= 8 && length <= 128;
+}
+
+function generatePassword(){
+
+  var passwordLength = prompt("Number of characters")
+  passwordLength = parseInt(passwordLength)
+
+  while (!validatePasswordLength(passwordLength)) {
+
+    passwordLength = prompt("Invalid number of characters")
+    passwordLength = parseInt(passwordLength)
+  }
+  var includeLowercase = confirm("Include lowercase letters?");
+  var includeUppercase = confirm("Include uppercase letters?");
+  var includeNumeric = confirm("Include numbers?");
+  var includeSpecial = confirm("Include special characters?")
+
+  if (!includeLowercase && !includeUppercase && !includeNumeric &&
+      !includeSpecial) {
+        alert("At least one criteria must be selected")
+      }
+
+
+
+}
