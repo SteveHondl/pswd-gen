eb2b495cdf61c5be23b95f67a303cae786637768
Steve Hondl <steve.hondl@gmail.com>
2023-11-11 21:22:06 -0600

    added var strings, added random function, added loop to include characters user has picked for password, added prompt

--- assets/script.js
+++ assets/script.js
@@ -14,32 +14,77 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // My code
-
+// password length restrictions
 function validatePasswordLength(length) {
     
   return length >= 8 && length <= 128;
 }
+// user prompted to pick a length for their password, parseInt will convert
+// the string and make sure it is a number
 
 function generatePassword(){
 
-  var passwordLength = prompt("Number of characters")
-  passwordLength = parseInt(passwordLength)
+  var passwordLength = prompt("Number of characters");
+  passwordLength = parseInt(passwordLength);
 
+  //requires user to put number in range of 8-128
+  //loop will continue until user meets requirements
   while (!validatePasswordLength(passwordLength)) {
 
-    passwordLength = prompt("Invalid number of characters")
-    passwordLength = parseInt(passwordLength)
+    passwordLength = prompt("Invalid number of characters");
+    passwordLength = parseInt(passwordLength);
   }
+
+  //include lowercase, uppercase, numbers, and special characters
   var includeLowercase = confirm("Include lowercase letters?");
   var includeUppercase = confirm("Include uppercase letters?");
   var includeNumeric = confirm("Include numbers?");
-  var includeSpecial = confirm("Include special characters?")
+  var includeSpecial = confirm("Include special characters?");
 
   if (!includeLowercase && !includeUppercase && !includeNumeric &&
       !includeSpecial) {
         alert("At least one criteria must be selected")
       }
 
+// end of user input section
+
+  //character choices
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "!@#$%^&*()-_=+[]{}|;:\,.<>/?";
+
+  //Initial password
+  var password = "";
+
+  function randomChars(characters) {
+    return characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+
+  for (var i = 0; i < passwordLength; i++) {
+    if (includeLowercase) {
+      password = password + randomChars(lowercaseChars);
+    }
+    if (includeUppercase) {
+      password = password + randomChars(uppercaseChars);
+    }
+    if (includeNumeric) {
+      password = password + randomChars(numericChars);
+    }
+    if (includeSpecial) {
+      password = password + randomChars(specialChars);
+    }
+  }
+    // Displays password in the alert tab
+    alert("Your generated password is: " + password);
+
+    //Displays password on the GUI 
+    return password;
+
+  }
 
 
-}
+
+
+  
+  
