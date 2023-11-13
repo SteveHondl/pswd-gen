1562a9d8e9958a9fc777910777bca66f29750b72
Steve Hondl <steve.hondl@gmail.com>
2023-11-12 18:22:04 -0600

    simplify code with more comments

--- assets/script.js
+++ assets/script.js
@@ -15,38 +15,39 @@ generateBtn.addEventListener("click", writePassword);
 
 // My code
 // password length restrictions
-function validatePasswordLength(length) {
+//length is the parameter
+  function validatePasswordLength(length) {
     
   return length >= 8 && length <= 128;
 }
-// user prompted to pick a length for their password, parseInt will convert
-// the string and make sure it is a number
+//variable is declared
+// user prompted to pick a length for their password
+  function generatePassword(){
 
-function generatePassword(){
+  var passwordLength = prompt("Number of characters?");
 
-  var passwordLength = prompt("Number of characters");
-  passwordLength = parseInt(passwordLength);
-
-  //requires user to put number in range of 8-128
-  //loop will continue until user meets requirements
+  //user required to pick a number of characters within the set parameter
+  //loop will continue until user is the in parameter
   while (!validatePasswordLength(passwordLength)) {
 
     passwordLength = prompt("Invalid number of characters");
-    passwordLength = parseInt(passwordLength);
+
   }
 
-  //include lowercase, uppercase, numbers, and special characters
+  //variables declared
+  //user is asked if they want to include each variable
   var includeLowercase = confirm("Include lowercase letters?");
   var includeUppercase = confirm("Include uppercase letters?");
   var includeNumeric = confirm("Include numbers?");
   var includeSpecial = confirm("Include special characters?");
 
+  //if none of the four variables are selected, the user will be alerted they must select at least one
   if (!includeLowercase && !includeUppercase && !includeNumeric &&
       !includeSpecial) {
         alert("At least one criteria must be selected")
       }
 
-// end of user input section
+  // end of user input section
 
   //character choices
   var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
@@ -54,13 +55,18 @@ function generatePassword(){
   var numericChars = "0123456789";
   var specialChars = "!@#$%^&*()-_=+[]{}|;:\,.<>/?";
 
-  //Initial password
+  //variable password is created and initialized with an empty string
   var password = "";
 
+  //function declared with characters being the parameter
+  //
   function randomChars(characters) {
     return characters.charAt(Math.floor(Math.random() * characters.length));
   }
-
+  
+  //this loop will take the length of characters the user has input,
+  //it will select random characters from the variables the user has chosen.
+  
   for (var i = 0; i < passwordLength; i++) {
     if (includeLowercase) {
       password = password + randomChars(lowercaseChars);
@@ -75,8 +81,6 @@ function generatePassword(){
       password = password + randomChars(specialChars);
     }
   }
-    // Displays password in the alert tab
-    alert("Your generated password is: " + password);
 
     //Displays password on the GUI 
     return password;
